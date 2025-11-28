fd4b0e58a95de22905b7c80b229585553096fc57
Subham Sangwan <subhamsangwan26@gmail.com>
2025-11-28 17:17:13 +0530

    feat: add mongoose connection and topic model

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import { ENV } from './config/env.js';
-
+import { connectDB } from './config/db.js';
+import topicRoutes from './routes/topic.route.js';
 
 const app = express();
 const PORT = ENV.PORT || 3000;
 
-
 app.use(express.json());
-app.get('/', (req,res) => {
-    res.send('API is running');
-})
+
+// Connect to Database
+connectDB();
+
+app.use('/api/topics', topicRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
