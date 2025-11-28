4f872fe6f9e76d47f4357ff21606b93a1c16ba27
AmanCrafts <theamanmalikarts@gmail.com>
2025-11-28 16:41:24 +0530

    feat: added server.js and related dependencies

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { ENV } from './config/env.js';
+
+
+const app = express();
+const PORT = ENV.PORT || 3000;
+
+
+app.use(express.json());
+app.get('/', (req,res) => {
+    res.send('API is running');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
